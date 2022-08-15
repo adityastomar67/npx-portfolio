57de8ba7c2a2b3c81abf6e34dea7868e39ce9ecc
Aditya Singh Tomar <adityastomar12@gmail.com>
2022-08-15 07:35:03 +0530

    Added formatting

--- bin/cli.js
+++ bin/cli.js
@@ -9,72 +9,72 @@ const path = require('path');
 const styledDataFilename = 'personal-info.json';
 
 try {
-  const res = fs.readFileSync(
-    path.resolve(__dirname, `../${styledDataFilename}`)
-  );
-  const infoData = JSON.parse(res);
+    const res = fs.readFileSync(
+        path.resolve(__dirname, `../${styledDataFilename}`)
+    );
+    const infoData = JSON.parse(res);
 
-  const {
-    first_name,
-    last_name,
-    job_title,
-    github_link,
-    twitter_link,
-    linkedin_link,
-    website_link,
-  } = infoData;
+    const {
+        first_name,
+        last_name,
+        job_title,
+        github_link,
+        twitter_link,
+        linkedin_link,
+        website_link,
+    } = infoData;
 
-  const styledData = {
-    firstName: chalk.yellowBright.bold.italic.underline(first_name),
-    lastName: chalk.yellow.bold.italic.underline(last_name),
+    const styledData = {
+        firstName: chalk.yellowBright.bold.italic.underline(first_name),
+        lastName: chalk.yellow.bold.italic.underline(last_name),
 
-    labelWork: chalk.white.bold('Work:'),
-    work: chalk.magenta(job_title),
+        labelWork: chalk.white.bold('Work:'),
+        work: chalk.magenta(job_title),
 
-    labelGitHub: chalk.white.bold('GitHub:'),
-    gitHub: chalk.cyan(github_link),
+        labelGitHub: chalk.white.bold('GitHub:'),
+        gitHub: chalk.cyan(github_link),
 
-    labelTwitter: chalk.white.bold('Twitter:'),
-    twitter: chalk.cyan(twitter_link),
+        labelTwitter: chalk.white.bold('Twitter:'),
+        twitter: chalk.cyan(twitter_link),
 
-    labelLinkedIn: chalk.white.bold('LinkedIn:'),
-    linkedIn: chalk.cyan(linkedin_link),
+        labelLinkedIn: chalk.white.bold('LinkedIn:'),
+        linkedIn: chalk.cyan(linkedin_link),
 
-    labelWebsite: chalk.white.bold('Website:'),
-    website: chalk.cyan(website_link),
+        labelWebsite: chalk.white.bold('Website:'),
+        website: chalk.cyan(website_link),
 
-    labelCard: chalk.white.bold('Card:'),
-    npxCard: chalk.white.underline('npx Portfolio'),
-  };
+        labelCard: chalk.white.bold('Card:'),
+        npxCard: chalk.white.underline('npx Portfolio'),
+    };
 
-  const newline = '\n';
-  const output =
-    newline +
-    `${styledData.firstName} ${styledData.lastName}` +
-    newline +
-    newline +
-    `${styledData.labelWork}  ${styledData.work}` +
-    newline +
-    `${styledData.labelGitHub}  ${styledData.gitHub}` +
-    newline +
-    `${styledData.labelTwitter}  ${styledData.twitter}` +
-    newline +
-    `${styledData.labelLinkedIn}  ${styledData.linkedIn}` +
-    newline +
-    `${styledData.labelWebsite}  ${styledData.website}` +
-    newline +
-    `${styledData.labelCard}  ${styledData.npxCard} `;
+    const newline = '\n';
+    const output =
+        newline +
+            `${styledData.firstName} ${styledData.lastName}` +
+            newline +
+            newline +
+            `${styledData.labelWork}  ${styledData.work}` +
+            newline +
+            `${styledData.labelGitHub}  ${styledData.gitHub}` +
+            newline +
+            `${styledData.labelTwitter}  ${styledData.twitter}` +
+            newline +
+            `${styledData.labelLinkedIn}  ${styledData.linkedIn}` +
+            newline +
+            `${styledData.labelWebsite}  ${styledData.website}` +
+            newline +
+            `${styledData.labelCard}  ${styledData.npxCard} `;
 
-  // The Boxen options
-  const options = {
-    padding: 1,
-    margin: 1,
-    borderStyle: 'round',
-    textAlignment: 'center',
-  };
+    // The Boxen options
+    const options = {
+        padding: 1,
+        margin: 1,
+        borderStyle: 'round',
+        textAlignment: 'center',
+    };
 
-  console.log(chalk.green(boxen(output, options)));
+    console.log(chalk.green(boxen(output, options)));
 } catch (err) {
-  console.log(chalk.bgRed.bold(`Cannot read "${styledDataFilename}" file!`));
-  console.log(chalk.italic(err.message));
+    console.log(chalk.bgRed.bold(`Cannot read "${styledDataFilename}" file!`));
+    console.log(chalk.italic(err.message));
 }
