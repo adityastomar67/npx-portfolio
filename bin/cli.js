96bb69d5f69eb45529b24c9f233325022787ebf8
Aditya Singh Tomar <adityastomar12@gmail.com>
2022-08-31 21:26:15 +0530

    Card v2.0.1

--- bin/cli.js
+++ bin/cli.js
@@ -1,80 +1,126 @@
 #!/usr/bin/env node
 
-const chalk = require('chalk');
-const boxen = require('boxen');
+'use strict'
 
+const boxen = require("boxen");
+const chalk = require("chalk");
+const inquirer = require("inquirer");
+const clear = require("clear");
+const open = require("open");
 const fs = require('fs');
+const request = require('request');
 const path = require('path');
+const ora = require('ora');
+const cliSpinners = require('cli-spinners');
+clear();
 
-const styledDataFilename = 'personal-info.json';
+const prompt = inquirer.createPromptModule();
 
-try {
-    const res = fs.readFileSync(
-        path.resolve(__dirname, `../${styledDataFilename}`)
-    );
-    const infoData = JSON.parse(res);
+const questions = [
+    {
+        type: "list",
+        name: "action",
+        message: "What you want to do?",
+        choices: [
+            {
+                name: `Send me an ${chalk.green.bold("email")}?`,
+                value: () => {
+                    open("mailto:adityastomar67@gmail.com");
+                    console.log("\nDone, see you soon at inbox.\n");
+                }
+            },
+            // For downloading and showing the resume as html file
+            // {
+            //     name: `Download my ${chalk.magentaBright.bold("Resume")}?`,
+            //     value: () => {
+            //         // cliSpinners.dots;
+            //         const loader = ora({
+            //             text: ' Downloading Resume',
+            //             spinner: cliSpinners.material,
+            //         }).start();
+            //         let pipe = request('https://#/api/resume').pipe(fs.createWriteStream('./aditya-resume.html'));
+            //         pipe.on("finish", function () {
+            //             let downloadPath = path.join(process.cwd(), 'anmol-resume.html')
+            //             console.log(`\nResume Downloaded at ${downloadPath} \n`);
+            //             open(downloadPath)
+            //             loader.stop();
+            //         });
+            //     }
+            // },
+            {
+                name: `Schedule a ${chalk.redBright.bold("Meeting")}?`,
+                value: () => {
+                    open('https://calendly.com/adityastomar67/30min');
+                    console.log("\n See you at the meeting \n");
+                }
+            },
+            {
+                name: "Just quit.",
+                value: () => {
+                    console.log("Hasta la vista.\n");
+                }
+            }
+        ]
+    }
+];
 
-    const {
-        first_name,
-        last_name,
-        job_title,
-        github_link,
-        twitter_link,
-        linkedin_link,
-        website_link,
-    } = infoData;
+const data = {
+    name: chalk.bold.green("                 Aditya Singh Tomar "),
+    handle: chalk.white("@adityastomar67"),
+    work: `${chalk.white("Software Engineer Intern")} ${chalk
+        .hex("#2b82b2")
+        .bold("Nagarro")}`,
+    twitter: chalk.gray("https://twitter.com/") + chalk.cyan("singhhhx"),
+    github: chalk.gray("https://github.com/") + chalk.green("adityastomar67"),
+    linkedin: chalk.gray("https://linkedin.com/in/") + chalk.blue("adityastomar67"),
+    web: chalk.cyan("#"),
+    npx: chalk.red("npx") + " " + chalk.white("adityastomar67"),
 
-    const styledData = {
-        firstName: chalk.yellowBright.bold.italic.underline(first_name),
-        lastName: chalk.yellow.bold.italic.underline(last_name),
+    labelWork: chalk.white.bold("       Work:"),
+    labelTwitter: chalk.white.bold("    Twitter:"),
+    labelGitHub: chalk.white.bold("     GitHub:"),
+    labelLinkedIn: chalk.white.bold("   LinkedIn:"),
+    labelWeb: chalk.white.bold("        Web:"),
+    labelCard: chalk.white.bold("       Card:")
+};
 
-        labelWork: chalk.white.bold('Work:'),
-        work: chalk.magenta(job_title),
-
-        labelGitHub: chalk.white.bold('GitHub:'),
-        gitHub: chalk.cyan(github_link),
-
-        labelTwitter: chalk.white.bold('Twitter:'),
-        twitter: chalk.cyan(twitter_link),
-
-        labelLinkedIn: chalk.white.bold('LinkedIn:'),
-        linkedIn: chalk.cyan(linkedin_link),
-
-        labelWebsite: chalk.white.bold('Website:'),
-        website: chalk.cyan(website_link),
-
-        labelCard: chalk.white.bold('Card:'),
-        npxCard: chalk.white.underline('npx Portfolio'),
-    };
-
-    const newline = '\n';
-    const output =
-        newline +
-            `${styledData.firstName} ${styledData.lastName}` +
-            newline +
-            newline +
-            `${styledData.labelWork}  ${styledData.work}` +
-            newline +
-            `${styledData.labelGitHub}  ${styledData.gitHub}` +
-            newline +
-            `${styledData.labelTwitter}  ${styledData.twitter}` +
-            newline +
-            `${styledData.labelLinkedIn}  ${styledData.linkedIn}` +
-            newline +
-            `${styledData.labelWebsite}  ${styledData.website}` +
-            newline +
-            `${styledData.labelCard}  ${styledData.npxCard} `;
-
-    // The Boxen options
-    const options = {
-        padding: 1,
+const me = boxen(
+    [
+        `                  ${data.name}`,
+        ``,
+        `                   ${data.labelWork}  ${data.work}`,
+        ``,
+        `                   ${data.labelTwitter}  ${data.twitter}`,
+        `                   ${data.labelGitHub}  ${data.github}`,
+        `                   ${data.labelLinkedIn}  ${data.linkedin}`,
+        `                   ${data.labelWeb}  ${data.web}`,
+        ``,
+        `                   ${data.labelCard}  ${data.npx}`,
+        ``,
+        `${chalk.italic(
+            "I am currently looking for new opportunities, my inbox is always open. Whether you have a"
+        )}`,
+        `${chalk.italic(
+            "        question or just want to say hi, I will try my best to get back to you!"
+        )}`,
+    ].join("\n"),
+    {
         margin: 1,
-        borderStyle: 'round',
-        textAlignment: 'center',
-    };
+        float: 'center',
+        padding: 1,
+        borderStyle: "single",
+        borderColor: "green"
+    }
+);
 
-    console.log(chalk.green(boxen(output, options)));
-} catch (err) {
-    console.log(chalk.bgRed.bold(`Cannot read "${styledDataFilename}" file!`));
-    console.log(chalk.italic(err.message));
-}
+console.log(me);
+const tip = [
+    `Tip: Try ${chalk.cyanBright.bold(
+        "cmd/ctrl + click"
+    )} on the links above`,
+    '',
+].join("\n");
+console.log(tip);
+
+prompt(questions).then(answer => answer.action());
+
